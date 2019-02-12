02bb36d24cb95525d3a39747ff100c95a552793a
Bruno FL <follet-bruno@hotmail.fr>
2019-02-12 13:34:22 +0100

    Adaptation pour oss

--- flexio-api-documentation-service/src/main/java/io/flexio/services/api/documentation/FlexioApiDocumentationApi.java
+++ flexio-api-documentation-service/src/main/java/io/flexio/services/api/documentation/FlexioApiDocumentationApi.java
@@ -2,16 +2,21 @@ package io.flexio.services.api.documentation;
 
 import io.flexio.services.api.documentation.RessourcesManager.FileSystemRessourcesManager;
 import io.flexio.services.api.documentation.handlers.*;
-import io.flexio.services.support.api.Api;
 import org.codingmatters.poom.services.support.Env;
 import org.codingmatters.rest.api.Processor;
 import com.fasterxml.jackson.core.JsonFactory;
 import io.flexio.services.api.documentation.api.FlexioApiDocumentationHandlers;
 import io.flexio.services.api.documentation.service.FlexioApiDocumentationProcessor;
 
-public class FlexioApiDocumentationApi implements Api {
+public class FlexioApiDocumentationApi {
 
     private final String name = "flexio-api-documentation";
+
+    public FlexioApiDocumentationApi(String apiPath) {
+        this.apiPath = apiPath;
+    }
+
+    private String apiPath;
     private FlexioApiDocumentationHandlers handlers;
     private FlexioApiDocumentationProcessor processor;
 
@@ -28,29 +33,20 @@ public class FlexioApiDocumentationApi implements Api {
                 .groupsGetHandler(new GetGroups(fs))
                 .modulesGetHandler(new GetModules(fs))
                 .versionsGetHandler(new GetVersions(fs))
-                            .build();
+                .build();
 
         this.processor = new FlexioApiDocumentationProcessor(this.path(), new JsonFactory(), handlers);
     }
 
-    @Override
     public Processor processor() {
         return this.processor;
     }
 
-    @Override
-    public String docResource() {
-        return "flexio-api-documentation.html";
+    public FlexioApiDocumentationHandlers getHandlers() {
+        return this.handlers;
     }
 
-    @Override
-    public String name() {
-        return this.name;
-    }
-
-    @Override
-    public String version() {
-        return "v2";
+    public String path() {
+        return apiPath;
     }
 }
-
--- flexio-api-documentation-service/src/main/java/io/flexio/services/api/documentation/FlexioApiDocumentationServices.java
+++ flexio-api-documentation-service/src/main/java/io/flexio/services/api/documentation/FlexioApiDocumentationServices.java
@@ -1,36 +1,53 @@
 package io.flexio.services.api.documentation;
 
-import io.flexio.services.metrics.HealthCheckProcessor;
-import io.flexio.services.support.api.Api;
-import io.flexio.services.support.api.ApiService;
-import io.flexio.services.support.args.Arguments;
-import io.flexio.services.support.args.ArgumentsException;
-import org.codingmatters.value.objects.values.ObjectValue;
+import com.fasterxml.jackson.core.JsonFactory;
+import io.undertow.Undertow;
+import org.codingmatters.poom.ci.pipeline.api.service.PoomCIApi;
+import org.codingmatters.poom.ci.pipeline.api.service.PoomCIPipelineService;
+import org.codingmatters.poom.services.support.Env;
+import org.codingmatters.rest.undertow.CdmHttpUndertowHandler;
 
-import static org.codingmatters.poom.services.support.Env.SERVICE_HOST;
-import static org.codingmatters.poom.services.support.Env.SERVICE_PORT;
+import java.io.File;
 
 public class FlexioApiDocumentationServices {
-    public static void main(String[] args){
-        try{
-            ApiService service = fromArguments(Arguments.parse(args));
-            service.go();
-        } catch (ArgumentsException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        System.exit(0);
+
+    private Undertow server;
+    private final int port;
+    private final String host;
+    private final FlexioApiDocumentationApi api;
+
+    public FlexioApiDocumentationServices(int port, String host, FlexioApiDocumentationApi api) {
+        this.port = port;
+        this.host = host;
+        this.api = api;
     }
 
-    public static ApiService fromArguments( Arguments arguments ) throws ArgumentsException {
-        Api api = null;
-        return new ApiService(
-                arguments.mandatoryString( SERVICE_HOST ),
-                arguments.mandatoryInteger( SERVICE_PORT ),
-                ()->{},
-                api
-        );
+    public static void main(String[] args){
+        String host = Env.mandatory(Env.SERVICE_HOST).asString();
+        int port = Env.mandatory(Env.SERVICE_PORT).asInteger();
+       // TODO start service
+        FlexioApiDocumentationServices service = new FlexioApiDocumentationServices(port, host, api());
+        service.start();
     }
+
+    static public FlexioApiDocumentationApi api(){
+        JsonFactory jsonFactory = new JsonFactory();
+
+        File logStorage = new File(Env.mandatory("LOG_STORAGE").asString());
+        String jobRegistryUrl = Env.mandatory("JOB_REGISTRY_URL").asString();
+
+        return new FlexioApiDocumentationApi();
+    }
+
+
+    public void start() {
+        this.server = Undertow.builder()
+                .addHttpListener(this.port, this.host)
+                .setHandler(new CdmHttpUndertowHandler(this.api.processor()))
+                .build();
+        this.server.start();
+    }
+
     
 }
 
